bffbd0cb052989267176df276564de33e3931fb6
Marcio Viana <vianalmarcio@gmail.com>
2022-04-27 11:49:43 -0300

    Catch ArithmeticException in fun dividir(); Change type of variables from Int to Double.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,34 @@
-fun somar(numero1: Int, numero2: Int): Int{
+fun somar(numero1: Double, numero2: Double): Double{
     val total = numero1 + numero2
     return total
 }
 
-fun subtrair(numero1:Int, numero2:Int): Int{
+fun subtrair(numero1:Double, numero2:Double): Double{
     val total = numero1 - numero2
     return total
 }
 
-fun multiplicar (numero1: Int, numero2: Int): Int{
+fun multiplicar (numero1: Double, numero2: Double): Double{
     val total = numero1 * numero2
     return total
 }
 
-fun dividir(numero1: Int, numero2: Int): Int{
-    val total = numero1/numero2
+fun dividir(numero1: Double, numero2: Double): Double{
+    var total: Double = 0.0
+    try {
+//        println("LINHA 19 - total antes: $total")
+        total = numero1/numero2
+//        println("LINHA 21 - total depois: $total")
+    } catch (e: ArithmeticException){
+        println("Não pode dividir número por zero.")
+    }
     return total
 }
 
 fun main() {
     val valorQueRepete = 13
-    val numero1 = 0
-    val numero2 = 2
+    val numero1 = 2.0
+    val numero2 = 5.0
 
     println("valor que repete é: $valorQueRepete")
     val resultado3 = somar(numero1, numero2)
@@ -32,4 +39,5 @@ fun main() {
     println(resultado1 + valorQueRepete)
     val resultado2 = dividir(numero1, numero2)
     println(resultado2 + valorQueRepete)
+    println(resultado2)
 }
